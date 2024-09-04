187dade6a1f76e8d1f678fd043f8ff9d2ac952ae
JonFdz <jonfernal@gmail.com>
2024-09-04 18:30:48 +0200

    Added static files serving routes

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import usersRouter from './routes/users.routes';
 import activitiesRouter from './routes/activities.routes';
 import inscriptionsRouter from './routes/inscriptions.routes';
 import env from './config/env';
+import path from 'path';
 
 const app: Application = express();
 
@@ -12,16 +13,23 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Static files
+app.use(express.static(path.join(__dirname, '../dist/public')));
+
 // Routes
 app.use('/api/users', usersRouter);
 app.use('/api/activities', activitiesRouter);
 app.use('/api/inscriptions', inscriptionsRouter);
 app.get('/api/hello', (req, res) => {
 	res.send('Hello World!');
-  });
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../dist/public', 'index.html'));
+});
 
 app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`);
+	console.log(`Server running on port ${env.PORT}`);
 });
 
 export default app;
